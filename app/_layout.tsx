7c67f7dabaa6ebf8c79cafa9742e49657b5c0c20
Andrea Acosta <acostaandreadev@gmail.com>
2025-04-23 13:06:40 -0300

    Implement authentication layout and enhance login screen with themed components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
-import { useColorScheme } from '@/presentation/theme/hooks/useColorScheme';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-
+import { useColorScheme } from "@/presentation/theme/hooks/useColorScheme";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import "react-native-reanimated";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -16,9 +19,9 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     // SpaceMono : require('../assets/fonts/SpaceMono-Regular.ttf'),
-    KanitBold: require('../assets/fonts/Kanit-Bold.ttf'),
-    KanitRegular: require('../assets/fonts/Kanit-Regular.ttf'),
-    KanitThin: require('../assets/fonts/Kanit-Thin.ttf'),
+    KanitBold: require("../assets/fonts/Kanit-Bold.ttf"),
+    KanitRegular: require("../assets/fonts/Kanit-Regular.ttf"),
+    KanitThin: require("../assets/fonts/Kanit-Thin.ttf"),
   });
 
   useEffect(() => {
@@ -32,15 +35,18 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack
-      screenOptions={{
-        headerShown: false,
-      }}>
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" /> */}
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
--- app/auth/login/index.tsx
+++ app/auth/login/index.tsx
@@ -1,13 +1,53 @@
-import { View,} from 'react-native'
-import React from 'react'
-import { ThemedText } from '@/presentation/theme/components/ThemedText'
+import {
+  KeyboardAvoidingView,
+  ScrollView,
+  useWindowDimensions,
+  View,
+} from "react-native";
+import React from "react";
+import { ThemedText } from "@/presentation/theme/components/ThemedText";
+import { TextInput } from "react-native-gesture-handler";
+import ThemedTextInput from "../../../presentation/theme/components/ThemedTextInput";
+import ThemedButton from "@/presentation/theme/components/ThemedButton";
+import ThemedLink from "@/presentation/theme/components/ThemedLink";
 
 const LoginScreen = () => {
+  const { height } = useWindowDimensions();
   return (
-    <View>
-      <ThemedText>LoginScreen</ThemedText>
-    </View>
-  )
-}
+    <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+      <ScrollView style={{ paddingHorizontal: 40 }}>
+        <View style={{ paddingTop: height * 0.35 }}>
+          <ThemedText type="title">Ingresar</ThemedText>
+          <ThemedText style={{ color: "gray" }}>
+            Por favor ingrese para continuar
+          </ThemedText>
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <ThemedTextInput
+            placeholder="Correo electrónico"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            icon="mail-outline"
+          />
+          <ThemedTextInput
+            placeholder="Contraseña"
+            secureTextEntry
+            autoCapitalize="none"
+            icon="lock-closed-outline"
+          />
+        </View>
+        <View style={{marginVertical:10}}/>
+        {/* Boton */}
+        <ThemedButton icon="arrow-forward-outline">Ingresar</ThemedButton>
+        <View style={{marginVertical:10}}/>
+        {/* Enlace */}
+        <View style={{flexDirection:'row',justifyContent:'center', alignItems:'center'}}>
+          <ThemedText>¿No tienes cuenta?</ThemedText>          
+          <ThemedLink href='/auth/register' style={{marginHorizontal:5}}>Crear cuenta</ThemedLink>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
